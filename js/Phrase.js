bc72afdb0c81ef30207cea19dc8964127261abb1
Matthew Grant <amattwgrant@gmail.com>
2020-08-19 16:13:57 -0400

    letters now displaying and assigned to proper classes

--- js/Phrase.js
+++ js/Phrase.js
@@ -4,4 +4,30 @@ class Phrase {
 	constructor(phrase) {
 		this.phrase = phrase.toLowerCase();
 	}
+
+	/**
+	 * Display phrase on game board
+	 */
+	 addPhraseToDisplay() {
+	 	let splitPhrase = this.phrase.split('');
+	 	const ul = document.querySelector('#phrase ul');
+	 	let currentCharacter = splitPhrase.forEach((letter) => {
+	 		const letters = /[a-z]/;
+	 		if ( letters.test(letter) === true ) {
+	 			let li = document.createElement('LI');
+	 			li.className = `hide letter ${letter}`;
+	 			li.textContent = `${letter}`;
+	 			ul.appendChild(li);
+	 			// alert(`${letter}`);
+	 		} else {
+	 			let li = document.createElement('LI');
+	 			li.className = "space";
+	 			li.textContent = ' ';
+	 			ul.appendChild(li);
+	 		} 
+	 	});
+
+	 }
+
 }
+
--- js/app.js
+++ js/app.js
@@ -1,3 +1,22 @@
 // handles all events for the two classes and runs the game
 
+// const game = new Game();
 
+// game.phrases.forEach((phrase, index) => {
+// 	console.log(`Phrase ${index} - phrase: ${phrase.phrase}`);
+// });
+
+// const logPhrase = (phrase) => {
+// 	console.log(`Phrase - phrase: `, phrase.phrase);
+// }
+
+// const game = new Game();
+
+// logPhrase(game.getRandomPhrase());
+
+
+const game = new Game();
+// game.getRandomPhrase().addPhraseToDisplay();
+const randomPhrase = game.getRandomPhrase();
+const phrase = new Phrase(randomPhrase.phrase);
+phrase.addPhraseToDisplay();
