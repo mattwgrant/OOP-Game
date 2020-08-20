a108f99c60a3c0c1a6b257e4ff7174d6af264394
Matthew Grant <amattwgrant@gmail.com>
2020-08-20 12:47:27 -0400

    startgame now working with button click

--- js/app.js
+++ js/app.js
@@ -18,6 +18,14 @@
 // const game = new Game();
 // game.getRandomPhrase().addPhraseToDisplay();
 
-const game = new Game();
-game.startGame();
-console.log(`Active Phrase - phrase: ${game.activePhrase.phrase}`);
+// const game = new Game();
+// game.startGame();
+// console.log(`Active Phrase - phrase: ${game.activePhrase.phrase}`);
+
+let game;
+
+document.getElementById('btn__reset').addEventListener('click', () => {
+	game = new Game();
+	game.startGame();
+});
+
