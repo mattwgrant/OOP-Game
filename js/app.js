9941bb3a621c063da0645ff0e0c788963a671d1b
Matthew Grant <amattwgrant@gmail.com>
2020-08-26 17:22:32 -0400

    cleaned up unnecessary code

--- js/app.js
+++ js/app.js
@@ -1,27 +1,5 @@
 // handles all events for the two classes and runs the game
 
-// const game = new Game();
-
-// game.phrases.forEach((phrase, index) => {
-// 	console.log(`Phrase ${index} - phrase: ${phrase.phrase}`);
-// });
-
-// const logPhrase = (phrase) => {
-// 	console.log(`Phrase - phrase: `, phrase.phrase);
-// }
-
-// const game = new Game();
-
-// logPhrase(game.getRandomPhrase());
-
-
-// const game = new Game();
-// game.getRandomPhrase().addPhraseToDisplay();
-
-// const game = new Game();
-// game.startGame();
-// console.log(`Active Phrase - phrase: ${game.activePhrase.phrase}`);
-
 let game;
 
 document.getElementById('btn__reset').addEventListener('click', () => {
